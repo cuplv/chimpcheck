84e339b06c4cd1c0016c9ed65e2c3e69575815f3
Soon Lee Edmund Lam <edmund.lam@colorado.edu>
2017-03-11 01:38:46 -0700

    ChimpDriver version 1.0 initial check in

---
+++ ChimpDriver/chimpDriver/src/main/java/edu/colorado/plv/chimp/driver/ChimpDriver.java
@@ -0,0 +1,176 @@
+package edu.colorado.plv.chimp.driver;
+
+import android.support.test.InstrumentationRegistry;
+import android.util.Base64;
+import android.util.Log;
+import chimp.protobuf.AppEventOuterClass;
+import chimp.protobuf.EventTraceOuterClass;
+import chimp.protobuf.ExtEventOuterClass;
+
+import static android.support.test.espresso.Espresso.onView;
+import static android.support.test.espresso.action.ViewActions.click;
+import static android.support.test.espresso.matcher.ViewMatchers.withId;
+
+/**
+ * Created by edmund on 3/10/17.
+ */
+public class ChimpDriver {
+
+    protected EventTraceOuterClass.EventTrace trace = null;
+    protected ChimpJUnitRunner runner = null;
+
+    protected void setEventTrace(EventTraceOuterClass.EventTrace trace) { this.trace = trace; }
+    protected void setRunner() { runner = (ChimpJUnitRunner) InstrumentationRegistry.getInstrumentation(); }
+
+    protected boolean isReady() { return trace != null && runner != null; }
+
+    protected void runTrace() {
+        if (runner == null) setRunner();
+        if (runner != null && trace == null) setEventTrace(runner.getEventTrace());
+        if (!isReady()) {
+            Log.e(runner.chimpTag("@runTrace"), "Chimp driver not ready.");
+            return;
+        }
+        for(EventTraceOuterClass.UIEvent event :trace.getEventsList()) {
+            executeEvent(event);
+        }
+
+        runner.addReport("Ran-Trace", Base64.encodeToString(trace.toByteArray(), Base64.DEFAULT));
+    }
+
+    // Execute Event methods. Implemented in a very sad way, because of how ProtoBufs handle union type.
+    // Better ideas are welcomed...
+
+    protected void executeEvent(EventTraceOuterClass.UIEvent event) {
+        switch (event.getEventType()) {
+            case APPEVENT: executeEvent(event.getAppEvent()); break;
+            case EXTEVENT: executeEvent(event.getExtEvent()); break;
+            case TRYEVENT: executeEvent(event.getTryEvent()); break;
+            case DECIDE: executeEvent(event.getDecide()); break;
+            case DECIDEMANY: executeEvent(event.getDecideMany()); break;
+        }
+    }
+
+    protected void executeEvent(AppEventOuterClass.AppEvent appevent) {
+        switch (appevent.getEventType()) {
+            case CLICK: executeEvent(appevent.getClick()); break;
+            case LONGCLICK: executeEvent( appevent.getLongclick() ); break;
+            case TYPE: executeEvent( appevent.getType() ); break;
+            case DRAG: executeEvent( appevent.getDrag() ); break;
+            case PINCH: executeEvent( appevent.getPinch() ); break;
+            case SWIPE: executeEvent( appevent.getSwipe() ); break;
+            case SLEEP: executeEvent( appevent.getSleep() ); break;
+        }
+    }
+
+    protected void executeEvent(ExtEventOuterClass.ExtEvent extevent) {
+        switch (extevent.getEventType()) {
+            case CLICKBACK: executeEvent( ExtEventOuterClass.ClickBack.getDefaultInstance() ); break;
+            case CLICKHOME: executeEvent( ExtEventOuterClass.ClickHome.getDefaultInstance() ); break;
+            case CLICKMENU: executeEvent( ExtEventOuterClass.ClickMenu.getDefaultInstance() ); break;
+            case PULLDOWNSETTINGS: executeEvent( ExtEventOuterClass.PullDownSettings.getDefaultInstance() ); break;
+            case RETURNTOAPP: executeEvent( ExtEventOuterClass.ReturnToApp.getDefaultInstance() ); break;
+            case ROTATELEFT:  executeEvent( ExtEventOuterClass.RotateLeft.getDefaultInstance() ); break;
+            case ROTATERIGHT: executeEvent( ExtEventOuterClass.RotateRight.getDefaultInstance() ); break;
+        }
+    }
+
+    // Try Event Block
+
+    protected void executeEvent(EventTraceOuterClass.TryEvent tryEvent) {
+        Log.i(runner.chimpTag("@executeEvent"), tryEvent.toString());
+        // TODO
+    }
+
+    protected void executeEvent(EventTraceOuterClass.Decide decide) {
+        Log.i(runner.chimpTag("@executeEvent"), decide.toString());
+        // TODO
+    }
+
+    protected void executeEvent(EventTraceOuterClass.DecideMany decideMany) {
+        Log.i(runner.chimpTag("@executeEvent"), decideMany.toString());
+        // TODO
+    }
+
+    // User Events
+
+    protected void testClick(int id) {
+        onView(withId(id)).perform(click());
+    }
+
+    protected void executeEvent(AppEventOuterClass.Click click) {
+       Log.i(runner.chimpTag("@executeEvent"), click.toString());
+       // TODO
+    }
+
+    protected void executeEvent(AppEventOuterClass.LongClick longClick) {
+        Log.i(runner.chimpTag("@executeEvent"), longClick.toString());
+        // TODO
+    }
+
+    protected void executeEvent(AppEventOuterClass.Type type) {
+        Log.i(runner.chimpTag("@executeEvent"), type.toString());
+        // TODO
+    }
+
+    protected void executeEvent(AppEventOuterClass.Drag drag) {
+        Log.i(runner.chimpTag("@executeEvent"), drag.toString());
+        // TODO
+    }
+
+    protected void executeEvent(AppEventOuterClass.Pinch pinch) {
+        Log.i(runner.chimpTag("@executeEvent"), pinch.toString());
+        // TODO
+    }
+
+    protected void executeEvent(AppEventOuterClass.Swipe swipe) {
+        Log.i(runner.chimpTag("@executeEvent"), swipe.toString());
+        // TODO
+    }
+
+    protected void executeEvent(AppEventOuterClass.Sleep sleep) {
+        Log.i(runner.chimpTag("@executeEvent"), sleep.toString());
+        // TODO
+    }
+
+
+    // External Events
+
+    protected void executeEvent(ExtEventOuterClass.ClickBack clickBack) {
+        Log.i(runner.chimpTag("@executeEvent"), clickBack.toString());
+        // TODO
+    }
+
+    protected void executeEvent(ExtEventOuterClass.ClickHome clickHome) {
+        Log.i(runner.chimpTag("@executeEvent"), clickHome.toString());
+        // TODO
+    }
+
+    protected void executeEvent(ExtEventOuterClass.ClickMenu clickMenu) {
+        Log.i(runner.chimpTag("@executeEvent"), clickMenu.toString());
+        // TODO
+    }
+
+    protected void executeEvent(ExtEventOuterClass.PullDownSettings pullDownSettings) {
+        Log.i(runner.chimpTag("@executeEvent"), pullDownSettings.toString());
+        // TODO
+    }
+
+    protected void executeEvent(ExtEventOuterClass.ReturnToApp returnToApp) {
+        Log.i(runner.chimpTag("@executeEvent"), returnToApp.toString());
+        // TODO
+    }
+
+    protected void executeEvent(ExtEventOuterClass.RotateLeft rotateLeft) {
+        Log.i(runner.chimpTag("@executeEvent"), rotateLeft.toString());
+        // TODO
+    }
+
+    protected void executeEvent(ExtEventOuterClass.RotateRight rotateRight) {
+        Log.i(runner.chimpTag("@executeEvent"), rotateRight.toString());
+        // TODO
+    }
+
+    public String test() { return "This chimp is wonky!"; }
+
+}
---
+++ ChimpDriver/chimpDriver/src/main/java/edu/colorado/plv/chimp/driver/ChimpJUnitRunner.java
@@ -0,0 +1,76 @@
+package edu.colorado.plv.chimp.driver;
+
+/**
+ * Created by edmund on 2/6/17.
+ */
+
+import android.os.Bundle;
+import android.support.test.runner.AndroidJUnitRunner;
+
+import android.util.Base64;
+import android.util.Log;
+import chimp.protobuf.*;
+import com.google.protobuf.InvalidProtocolBufferException;
+
+import java.util.HashMap;
+import java.util.Map;
+
+public class ChimpJUnitRunner extends AndroidJUnitRunner {
+
+    private static final String TAG = null;
+    private EventTraceOuterClass.EventTrace eventTrace;
+    private String chimpName;
+    private boolean inputIsWelformed = true;
+
+    private Map<String, String> chimpReports = new HashMap<String, String>();
+
+    /* (non-Javadoc)
+     * @see android.test.InstrumentationTestRunner#onCreate(android.os.Bundle)
+     */
+    @Override
+    public void onCreate(Bundle arguments) {
+        super.onCreate(arguments);
+
+        if (arguments != null) {
+            String base64EncodedEventTrace = arguments.getString("eventTrace");
+            try {
+                eventTrace = EventTraceOuterClass.EventTrace.parseFrom(
+                        Base64.decode(base64EncodedEventTrace, Base64.DEFAULT)
+                );
+            } catch (InvalidProtocolBufferException e) {
+                inputIsWelformed = false;
+                Log.e("ChimpDriver",String.format("InvalidProtocolBufferException: %s", e.toString()));
+            } catch (Exception e) {
+                inputIsWelformed = false;
+                Log.e("ChimpDriver",String.format("Unknown Exception during eventTrace extraction: %s", e.toString()));
+            }
+
+            chimpName = arguments.getString("chimpName", "Caesar");
+        }
+    }
+
+    @Override
+    public void finish(int code, Bundle bundle) {
+        // bundle.putString("chimp-check", "Little monkeys.");
+        // bundle.putString("##EeeeWah", "The chimp is here!");
+        for(Map.Entry<String,String> entry : chimpReports.entrySet()) {
+            bundle.putString(entry.getKey(), entry.getValue());
+        }
+        super.finish(code, bundle);
+    }
+
+    public boolean inputIsWelformed() { return inputIsWelformed; }
+
+    public EventTraceOuterClass.EventTrace getEventTrace() {
+        return eventTrace;
+    }
+
+    public void addReport(String key, String data) { chimpReports.put(key, data); }
+
+    public String chimpTag(String tag) { return String.format("%s:%s",chimpName, tag); }
+
+    public void printOnLogCat(String tag, String data) {
+        Log.i(String.format("%s:%s",chimpName, tag), data);
+    }
+
+}
