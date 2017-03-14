1c8049f3399a48b5a82d3e1c6e652e31094b6bdc
Soon Lee Edmund Lam <edmund.lam@colorado.edu>
2017-03-14 01:10:48 -0600

    Added result parsing

--- ChimpCombinator/src/main/scala/edu/colorado/plv/chimp/coordinator/ChimpLoader.scala
+++ ChimpCombinator/src/main/scala/edu/colorado/plv/chimp/coordinator/ChimpLoader.scala
@@ -3,16 +3,19 @@ package edu.colorado.plv.chimp.coordinator
 import chimp.{protobuf => pb}
 import com.typesafe.scalalogging.Logger
 import edu.colorado.plv.chimp.combinator.EventTrace
+import edu.colorado.plv.fixr.bash.{Fail, Succ}
 import edu.colorado.plv.fixr.bash.android.{Aapt, Adb, AmInstrument, Emulator}
-import edu.colorado.plv.fixr.bash.utils.doTry
+import edu.colorado.plv.fixr.bash.utils.{FailTry, SuccTry, doTry}
 
 /**
   * Created by edmund on 3/11/17.
   */
 object ChimpLoader {
 
+  def extractValueWithKey(key:String, line:String): String = line.split(key)(1).drop(1).trim()
+
   def quickLoad(emuID:String, eventTrace: EventTrace, appAPKPath:String, chimpAPKPath:String, testerClass:String, aaptHomePath:String)
-               (implicit bashLogger: Logger): Unit = {
+               (implicit bashLogger: Logger): Option[ChimpTraceResults] = {
     val b64ProtoTrace = eventTrace.toBase64()
 
     val instrOut = for{
@@ -30,7 +33,34 @@ object ChimpLoader {
         .components( appInfo.packageName, testerClass, testInfo.packageName,"edu.colorado.plv.chimp.driver.ChimpJUnitRunner") !!!
     } yield instrOut
 
-    println(s"Done! $instrOut")
+    // println(s"Done! $instrOut")
+
+    instrOut match {
+      case SuccTry(o) => {
+         var results:Option[Boolean] = None
+         var trace:Option[EventTrace] = None
+         for(seg <- o.replace("\n","").split("INSTRUMENTATION")) {
+           if(seg contains "ChimpTraceResult") {
+             extractValueWithKey("ChimpTraceResult", seg) match {
+               case "Success" => results = Some(true)
+               case "Failed"  => results = Some(false)
+               case default   =>
+             }
+           }
+           if(seg contains "ChimpTraceCompleted") {
+             // val trstr = extractValueWithKey("ChimpTraceCompleted", seg)
+             // println(s"Here it is: $trstr")
+             trace = Some( EventTrace.fromBase64( extractValueWithKey("ChimpTraceCompleted", seg) ) )
+           }
+         }
+        (trace,results) match {
+          case (Some(tr),Some(res)) => Some( ChimpTraceResults(tr,res) )
+          case default => None
+        }
+      }
+      case FailTry(Fail(c, ec, o, e)) => None
+    }
+
   }
 
 }
