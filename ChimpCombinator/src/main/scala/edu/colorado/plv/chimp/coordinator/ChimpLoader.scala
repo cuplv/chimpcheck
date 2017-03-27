95c1eed5a7560366b373516e0ea3b5947147900f
Soon Lee Edmund Lam <edmund.lam@colorado.edu>
2017-03-26 23:43:36 -0600

    added kick back routine

--- ChimpCombinator/src/main/scala/edu/colorado/plv/chimp/coordinator/ChimpLoader.scala
+++ ChimpCombinator/src/main/scala/edu/colorado/plv/chimp/coordinator/ChimpLoader.scala
@@ -1,11 +1,11 @@
 package edu.colorado.plv.chimp.coordinator
 
-import chimp.{protobuf => pb}
 import com.typesafe.scalalogging.Logger
 import edu.colorado.plv.chimp.combinator.EventTrace
-import edu.colorado.plv.fixr.bash.{Fail, Succ}
+import edu.colorado.plv.fixr.bash.{Cmd, Fail, Lift, Succ}
 import edu.colorado.plv.fixr.bash.android.{Aapt, Adb, AmInstrument, Emulator}
-import edu.colorado.plv.fixr.bash.utils.{FailTry, SuccTry, doTry}
+import edu.colorado.plv.fixr.bash.utils.{FailTry, SuccTry, doTry, repeat}
+import org.slf4j.LoggerFactory
 
 /**
   * Created by edmund on 3/11/17.
@@ -61,6 +61,60 @@ object ChimpLoader {
       case FailTry(Fail(c, ec, o, e)) => None
     }
 
+  }
+
+  def kickBack(emuID:String, appPackageName:String) (implicit bashLogger:Logger): Unit = {
+    for {
+    // Wait until the app is first started
+      p <- repeat (Adb.target(emuID).shell("ps") #| Cmd(s"grep $appPackageName")) until {
+        case SuccTry(Succ(c, o, e)) => c.split("\n").length > 0
+        case default => false
+      }
+    } yield p
+
+    bashLogger.info("App started, KickBack routine now active...")
+
+    while(true) {
+      Adb.shellPsGrep(appPackageName) match {
+        case SuccTry(ls) => {
+          if (ls.length == 0) {
+            bashLogger.info("App has terminated, KickBack terminating..")
+            return
+          }
+        }
+        case default => {
+          bashLogger.info("App has terminated, KickBack terminating..")
+          return
+        }
+      }
+      Adb.getForeGroundAppName match {
+        case SuccTry(appName) => {
+          if (appName != appPackageName) {
+            bashLogger.info("App exited! Kicking back now!")
+            for {
+              p0 <- Lift !!! Thread.sleep(1500) ;
+              p1 <- Adb.shell("input keyevent KEYCODE_APP_SWITCH") ! ;
+              p2 <- Lift !!! Thread.sleep(2000) ;
+              p3 <- Adb.shell("input keyevent KEYCODE_DPAD_DOWN") ! ;
+              p4 <- Lift !!! Thread.sleep(2000) ;
+              p5 <- Adb.shell("input keyevent KEYCODE_ENTER") !
+            } yield p5
+          }
+        }
+        case default =>
+      }
+      Thread.sleep(2000)
+    }
+
+  }
+
+}
+
+object TestKickBack {
+
+  def main(args:Array[String]): Unit = {
+    implicit val logger = Logger(LoggerFactory.getLogger("kick-back-tester"))
+    ChimpLoader.kickBack("emulator-5554", "com.peilunzhang.contractiontimerdistilled")
   }
 
 }
