f08f8df67b5f4eff0349b1954967d5abfab0fb70
Soon Lee Edmund Lam <edmund.lam@colorado.edu>
2017-03-30 00:24:21 -0600

    added property combinators

--- ChimpCombinator/src/main/scala/edu/colorado/plv/chimp/combinator/EventTraces.scala
+++ ChimpCombinator/src/main/scala/edu/colorado/plv/chimp/combinator/EventTraces.scala
@@ -5,9 +5,10 @@ package edu.colorado.plv.chimp.combinator
   */
 
 import chimp.protobuf.Orientation.OrientType.{DOWN, LEFT, RIGHT, UP, XY_TYPE}
-import chimp.protobuf.{Orientation, UIEvent, UIID}
 import chimp.{protobuf => pb}
-import edu.colorado.plv.chimp.combinator.TryEvent_Implicits.{TryAppEvent, TryExtEvent, TryTrace}
+import Prop_Implicits.LitProp
+import TryEvent_Implicits.{TryAppEvent, TryExtEvent, TryTrace}
+import edu.colorado.plv.chimp.combinator.PropArg_Implicits.{BoolArg, IntArg, StrArg}
 import edu.colorado.plv.chimp.utils.Base64
 import org.scalacheck.Prop.False
 
@@ -87,6 +88,10 @@ object UIEvent {
          case pb.UIEvent.UIEventType.DECIDEMANY => {
              DecideMany( event.getDecideMany.alternatives.map(Alternative.fromProto(_)):_* )
          }
+
+         case pb.UIEvent.UIEventType.ASSERT => {
+             Assert( Prop.fromProto(event.getAssert.props) )
+         }
       }
    }
 }
@@ -99,24 +104,7 @@ abstract class UIEvent extends ProtoMsg[pb.UIEvent] {
 
 abstract class UIID extends ProtoMsg[pb.UIID]
 
-object UIID_Implicits {
 
-   implicit class RId(rid: Int) extends UIID {
-      override def toMsg(): pb.UIID = pb.UIID(pb.UIID.UIIDType.R_ID, Some(rid), None)
-      override def toString(): String = s"$rid"
-   }
-
-   implicit class Name(name: String) extends UIID {
-      override def toMsg(): pb.UIID = pb.UIID(pb.UIID.UIIDType.NAME_ID, None, Some(name))
-      override def toString(): String = name
-   }
-
-   implicit class XY(xy: Coord) extends UIID {
-      override def toMsg(): pb.UIID = pb.UIID(pb.UIID.UIIDType.XY_ID, None, None, Some(xy.toMsg()))
-      override def toString: String = s"<${xy.x},${xy.y}>"
-   }
-
-}
 
 object * extends UIID {
    override def toMsg(): pb.UIID = pb.UIID(pb.UIID.UIIDType.WILD_CARD, None, None)
@@ -151,14 +139,6 @@ case class Coord(x:Int,y:Int) extends ProtoMsg[pb.XYCoordin] {
 
 abstract class Orientation extends ProtoMsg[pb.Orientation]
 
-object Orient_Implicits {
-
-   implicit class XYOrient(xy : Coord) extends Orientation {
-      override def toMsg(): pb.Orientation = pb.Orientation( pb.Orientation.OrientType.XY_TYPE, Some(xy.toMsg()) )
-      override def toString: String = xy.toString
-   }
-
-}
 
 object Left extends Orientation {
    override def toMsg(): pb.Orientation = pb.Orientation(pb.Orientation.OrientType.LEFT, None)
@@ -267,28 +247,6 @@ abstract class TryEvent {
    def isTrace(): Boolean
 }
 
-object TryEvent_Implicits {
-
-   implicit class TryAppEvent(appevent: AppEvent) extends TryEvent {
-      override def event(): UIEvent = appevent
-      override def trace(): EventTrace = null
-      override def isTrace(): Boolean = false
-   }
-
-   implicit class TryExtEvent(extevent: ExtEvent) extends TryEvent {
-      override def event(): UIEvent = extevent
-      override def trace(): EventTrace = null
-      override def isTrace(): Boolean = false
-   }
-
-   implicit class TryTrace(tr: EventTrace) extends TryEvent {
-      override def event(): UIEvent = null
-      override def trace(): EventTrace = tr
-      override def isTrace(): Boolean = true
-   }
-
-}
-
 case class Try(event: TryEvent) extends UIEvent {
    override def toMsg(): pb.UIEvent = {
       if (!event.isTrace()) {
@@ -358,6 +316,10 @@ case class DecideMany(alts: Alternative*) extends UIEvent {
    def add(alt: Alternative): DecideMany = DecideMany( (alt +: alts):_*  )
 }
 
+case class Assert(prop: Prop) extends UIEvent {
+   override def toMsg(): pb.UIEvent = ProtoMsg.mkUIEvent( pb.Assert(prop.toMsg()) )
+}
+
 
 object ExtEvent {
    def fromProto(extevent: pb.ExtEvent): ExtEvent = {
@@ -414,3 +376,124 @@ object Rotate extends ExtEvent {
    }
    override def toString: String = "Rotate"
 }
+
+// http://www.mkyong.com/java/how-to-use-reflection-to-call-java-method-at-runtime/
+
+// Properties
+
+object PropArg {
+   def fromProto(arg: pb.Arg): PropArg = {
+      arg.argType match {
+         case pb.Arg.ArgType.STR_ARG  => new StrArg(arg.getStrVal)
+         case pb.Arg.ArgType.INT_ARG  => new IntArg(arg.getIntVal)
+         case pb.Arg.ArgType.BOOL_ARG => new BoolArg(arg.getBoolVal)
+      }
+   }
+}
+
+abstract class PropArg extends ProtoMsg[pb.Arg] {
+   val argType: PropArgType
+
+   abstract class PropArgType
+   object StrArgType extends PropArgType
+   object IntArgType extends PropArgType
+   object BoolArgType extends PropArgType
+}
+
+
+
+object BaseProp {
+   def fromProto(pbBaseProp: pb.BaseProp): BaseProp = {
+      pbBaseProp.propType match {
+         case pb.BaseProp.BasePropType.PRIM_TYPE => {
+            val pred = pbBaseProp.getPred
+            Predicate(pred.name, pred.args.map(PropArg.fromProto(_)):_* )
+         }
+         case pb.BaseProp.BasePropType.CONJ_BASE_TYPE => {
+            BaseConjunct( BaseProp.fromProto(pbBaseProp.getProp1), BaseProp.fromProto(pbBaseProp.getProp2) )
+         }
+         case pb.BaseProp.BasePropType.DISJ_BASE_TYPE => {
+            BaseDisjunct( BaseProp.fromProto(pbBaseProp.getProp1), BaseProp.fromProto(pbBaseProp.getProp2) )
+         }
+      }
+   }
+}
+
+abstract class BaseProp extends ProtoMsg[pb.BaseProp] {
+   def /\ (baseProp: BaseProp): BaseProp = BaseConjunct(this, baseProp)
+   def \/ (baseProp: BaseProp): BaseProp = BaseDisjunct(this, baseProp)
+   def /\ (prop: Prop): Prop = Conjunct(this, prop)
+   def \/ (prop: Prop): Prop = Disjunct(this, prop)
+   def ==> (baseProp: BaseProp): Prop = ImpProp(this, baseProp)
+}
+
+case class Predicate(name:String, args: PropArg*) extends BaseProp {
+   override def toMsg(): pb.BaseProp = {
+      val pbArgs = args.map( _.toMsg() )
+      val pbPred = pb.Pred(name,pbArgs)
+      pb.BaseProp(pb.BaseProp.BasePropType.PRIM_TYPE, Some(pbPred))
+   }
+   override def toString: String = s"$name(${args.mkString(",")})"
+}
+
+case class BaseConjunct(b1: BaseProp, b2: BaseProp) extends BaseProp {
+   override def toMsg(): pb.BaseProp = {
+      pb.BaseProp(pb.BaseProp.BasePropType.CONJ_BASE_TYPE, None, Some(b1.toMsg()), Some(b2.toMsg()))
+   }
+   override def toString: String = s"$b1 /\\ $b2"
+}
+
+case class BaseDisjunct(b1: BaseProp, b2: BaseProp) extends BaseProp {
+   override def toMsg(): pb.BaseProp = {
+      pb.BaseProp(pb.BaseProp.BasePropType.DISJ_BASE_TYPE, None, Some(b1.toMsg()), Some(b2.toMsg()))
+   }
+   override def toString: String = s"$b1 \\/ $b2"
+}
+
+object Prop {
+
+   def fromProto(pbProp: pb.Prop): Prop = {
+      pbProp.propType match {
+         case pb.Prop.PropType.LIT_TYPE => new LitProp( BaseProp.fromProto(pbProp.getPrem) )
+         case pb.Prop.PropType.IMP_TYPE => {
+            ImpProp( BaseProp.fromProto(pbProp.getPrem), BaseProp.fromProto(pbProp.getConc) )
+         }
+         case pb.Prop.PropType.CONJ_TYPE => {
+            Conjunct( Prop.fromProto(pbProp.getProp1), Prop.fromProto(pbProp.getProp2) )
+         }
+         case pb.Prop.PropType.DISJ_TYPE => {
+            Disjunct( Prop.fromProto(pbProp.getProp1), Prop.fromProto(pbProp.getProp2) )
+         }
+      }
+   }
+
+}
+
+abstract class Prop extends ProtoMsg[pb.Prop] {
+   def /\ (prop: Prop): Prop = Conjunct(this, prop)
+   def \/ (prop: Prop): Prop = Disjunct(this, prop)
+}
+
+
+
+case class ImpProp(prem: BaseProp, conc: BaseProp) extends Prop {
+   override def toMsg(): pb.Prop = {
+      pb.Prop(pb.Prop.PropType.IMP_TYPE, Some(prem.toMsg()), Some(conc.toMsg()))
+   }
+   override def toString: String = s"$prem ==> $conc"
+}
+
+case class Conjunct(p1:Prop, p2:Prop) extends Prop {
+   override def toMsg(): pb.Prop = {
+      pb.Prop(pb.Prop.PropType.CONJ_TYPE, None, None, Some(p1.toMsg()), Some(p2.toMsg()))
+   }
+   override def toString: String = s"$p1 /\\ $p2"
+}
+
+case class Disjunct(p1:Prop, p2:Prop) extends Prop {
+   override def toMsg(): pb.Prop = {
+      pb.Prop(pb.Prop.PropType.DISJ_TYPE, None, None, Some(p1.toMsg()), Some(p2.toMsg()))
+   }
+   override def toString: String = s"$p1 \\/ $p2"
+}
+
