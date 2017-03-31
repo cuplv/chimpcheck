d88edee7eec5dbe39ab2a36aeeab4f727bd12037
Soon Lee Edmund Lam <edmund.lam@colorado.edu>
2017-03-31 16:50:49 -0600

    added builtin properties

--- ChimpCombinator/src/main/scala/edu/colorado/plv/chimp/combinator/BuiltinProperties.scala
+++ ChimpCombinator/src/main/scala/edu/colorado/plv/chimp/combinator/BuiltinProperties.scala
@@ -12,32 +12,26 @@ abstract class ViewID {
   def toArg: PropArg
 }
 
-object ViewID_Implicits {
-
-  implicit class ViewNameID(name: String) extends ViewID {
-     def toArg(): PropArg = new StrArg(name)
-     override def toString: String = name
-  }
-
-  implicit class ViewRID(rid: Int) extends ViewID {
-     def toArg(): PropArg = new IntArg(rid)
-     override def toString: String = s"$rid"
-  }
-
-}
-
 case class isClickable(vID: ViewID) extends BaseProp {
   override def toMsg(): pb.BaseProp = Predicate("isClickable",vID.toArg).toMsg()
+}
+
+case class isDisplayed(vID: ViewID) extends BaseProp {
+  override def toMsg(): pb.BaseProp = Predicate("isDisplayed",vID.toArg).toMsg()
 }
 
 case class isEnabled(vID: ViewID) extends BaseProp {
   override def toMsg(): pb.BaseProp = Predicate("isEnabled", vID.toArg).toMsg()
 }
 
-case class supportInputMethods(vID: ViewID) extends BaseProp {
-  override def toMsg(): pb.BaseProp = Predicate("supportInputMethods", vID.toArg).toMsg()
+case class supportsInputMethods(vID: ViewID) extends BaseProp {
+  override def toMsg(): pb.BaseProp = Predicate("supportsInputMethods", vID.toArg).toMsg()
+}
+
+case class isSelected(vID: ViewID) extends BaseProp {
+  override def toMsg(): pb.BaseProp = Predicate("isSelected",vID.toArg).toMsg()
 }
 
 object dummy extends BaseProp {
-  override def toMsg(): pb.BaseProp = Predicate("dummy", Seq():_*).toMsg();
+  override def toMsg(): pb.BaseProp = Predicate("dummy", Seq():_*).toMsg()
 }
