86a5ecdbe059cd13fba8ef502428fb2b232f7c8a
Soon Lee Edmund Lam <edmund.lam@colorado.edu>
2017-04-01 23:27:51 -0600

    Added basic counting

--- examples/ContractionTimerDistilled/ContractionTimerDistilled-App/app/src/main/java/com/peilunzhang/contractiontimerdistilled/MainActivity.java
+++ examples/ContractionTimerDistilled/ContractionTimerDistilled-App/app/src/main/java/com/peilunzhang/contractiontimerdistilled/MainActivity.java
@@ -3,11 +3,13 @@ package com.peilunzhang.contractiontimerdistilled;
 import android.app.FragmentManager;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
 
+    int count = 0;
 
     FragmentManager fragmentManager = getFragmentManager();
 
@@ -23,6 +25,7 @@ public class MainActivity extends AppCompatActivity {
 
             @Override
             public void onClick(View view){
+                count++;
                 fragmentManager.beginTransaction().
                         replace(R.id.fragment_container, new CountdownFragment()).commit();
             };
@@ -30,13 +33,25 @@ public class MainActivity extends AppCompatActivity {
         secondFragmentBtn.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view){
+                count++;
                 fragmentManager.beginTransaction().
                         replace(R.id.fragment_container, new EmptyFragment()).commit();
             };
         });
 
+        Log.i("ContractionTimer-App","On Create: count is now " + count);
+
         // String[] perms = { "android.permission.READ_EXTERNAL_STORAGE", "android.permission.WRITE_EXTERNAL_STORAGE" };
         // requestPermissions(perms,200);
 
     }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+        count++;
+        Log.i("ContractionTimer-App","On Resume: count is now " + count);
+    }
+
+    public int getCount() { return count; }
 }
