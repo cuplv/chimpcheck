90ace275fb3361d64789f5e61b955a5cd1f99783
Edmund Lam <edmundlam@engr2-0-102-dhcp.int.colorado.edu>
2017-05-03 11:28:20 -0600

    updated refactor classes (currently unused, refactoring de-prioritized)

--- ChimpDriver/chimpDriver/src/main/java/edu/colorado/plv/chimp/managers/ViewManager.java
+++ ChimpDriver/chimpDriver/src/main/java/edu/colorado/plv/chimp/managers/ViewManager.java
@@ -1,7 +1,13 @@
 package edu.colorado.plv.chimp.managers;
 
+import java.util.ArrayList;
+
+import chimp.protobuf.AppEventOuterClass;
+import edu.colorado.plv.chimp.utils.ViewOption;
+
 /**
  * Created by edmund on 5/2/17.
  */
 public class ViewManager {
+
 }
--- ChimpDriver/chimpDriver/src/main/java/edu/colorado/plv/chimp/utils/UIIDInterp.java
+++
@@ -1,7 +0,0 @@
-package edu.colorado.plv.chimp.utils;
-
-/**
- * Created by edmund on 5/2/17.
- */
-public class UIIDInterp {
-}
