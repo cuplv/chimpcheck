979a4ea8d6cb74ff4e7a66b7508e4995425ef6a9
Edmund Lam <edmundlam@engr2-3-27-dhcp.int.colorado.edu>
2017-05-15 10:26:17 -0600

    added examples

--- examples/ChimpSample1/ChimpChecker/src/main/scala/edu/colorado/plv/chimp/example/TestApp.scala
+++ examples/ChimpSample1/ChimpChecker/src/main/scala/edu/colorado/plv/chimp/example/TestApp.scala
@@ -1,19 +1,93 @@
 package edu.colorado.plv.chimp.example
 
+import akka.actor.ActorSystem
 import plv.colorado.edu.chimpsample.R
-import com.typesafe.scalalogging.Logger
 import edu.colorado.plv.chimp.combinator.{Assert, Rotate, _}
 import edu.colorado.plv.chimp.combinator.Implicits._
 import edu.colorado.plv.chimp.generator.Implicits._
-import edu.colorado.plv.chimp.coordinator.ChimpLoader
-import edu.colorado.plv.chimp.generator.Gorilla
+import edu.colorado.plv.chimp.coordinator.{ChimpConfig, ChimpContext, ChimpLoader}
+import edu.colorado.plv.chimp.generator.{Gorilla, SubservientGorilla}
+import edu.colorado.plv.chimp.coordinator.MissionControl._
+import org.scalacheck.Prop._
+import org.scalacheck.Prop.forAll
+import org.scalacheck.Test.Parameters
+import com.typesafe.scalalogging.Logger
 import org.slf4j.LoggerFactory
 
 import scala.concurrent.ExecutionContext
+import scala.concurrent.duration._
 
 /**
   * Created by edmund on 3/11/17.
   */
+object RunBasic {
+
+  def main(args: Array[String]): Unit = {
+
+    implicit val system = ActorSystem("my-system")
+    implicit val executionContext = system.dispatcher
+
+    // IMPORTANT !! Reset this to your local path to the ChimpSample1 APKs.
+    val apkDir = "/data/chimp/ChimpSample1"
+    // IMPORTANT !! Reset this to your Android SDK path. You will also need to ensure that Aapt 24.0.3 is available.
+    val androidSDKHome = "/Users/edmundlam/Library/Android/sdk"
+
+    val config = ChimpConfig.defaultConfig().withStartEmulator(false).withTestRun(false).withTimeout(720 seconds)
+      // .addDeviceInfo(DeviceInfo("emulator-5556"))
+      // .addDeviceInfo(DeviceInfo("emulator-5558"))
+      // .addDeviceInfo(DeviceInfo("emulator-5560"))
+      .withAPKs(s"$apkDir/app-debug.apk", s"$apkDir/app-debug-androidTest.apk", "TestExpresso")
+      .withAaptHome(s"$androidSDKHome/build-tools/24.0.3")
+
+    implicit val chimpContext = ChimpContext.initDefaultChimpContext(config)
+
+    val myParam = Parameters.default.withMinSuccessfulTests(1).withWorkers(1)
+
+    forAll(Gorilla.generator) {
+      tr => tr chimpCheck { true }
+    }.check(myParam)
+
+  }
+
+}
+
+object RunCustom {
+
+  def main(args: Array[String]): Unit = {
+
+    implicit val system = ActorSystem("my-system")
+    implicit val executionContext = system.dispatcher
+
+    // IMPORTANT !! Reset this to your local path to the ChimpSample1 APKs.
+    val apkDir = "/data/chimp/ChimpSample1"
+    // IMPORTANT !! Reset this to your Android SDK path. You will also need to ensure that Aapt 24.0.3 is available.
+    val androidSDKHome = "/Users/edmundlam/Library/Android/sdk"
+
+    val config = ChimpConfig.defaultConfig().withStartEmulator(false).withTestRun(false).withTimeout(720 seconds)
+      // .addDeviceInfo(DeviceInfo("emulator-5556"))
+      // .addDeviceInfo(DeviceInfo("emulator-5558"))
+      // .addDeviceInfo(DeviceInfo("emulator-5560"))
+      .withAPKs(s"$apkDir/app-debug.apk", s"$apkDir/app-debug-androidTest.apk", "TestExpresso")
+      .withAaptHome(s"$androidSDKHome/build-tools/24.0.3")
+
+    implicit val chimpContext = ChimpContext.initDefaultChimpContext(config)
+
+    implicit val gorillaConfig = Gorilla.defaultGorillaConfig
+
+    val myParam = Parameters.default.withMinSuccessfulTests(1).withWorkers(1)
+
+    val loginSeq = Type(R.id.input_username,"testuser") :>> Type(R.id.input_password,"1234") :>> Click(R.id.btn_login)
+
+    val custom = Click(R.id.button_begin) :>> SubservientGorilla( isDisplayed(R.id.btn_login) Then (loginSeq :>> Assert(isDisplayed(R.id.interm_btn_back))) )
+
+    forAll(custom.generator) {
+      tr => tr chimpCheck { true }
+    }.check(myParam)
+
+  }
+
+}
+
 object TestApp {
 
   def main(args: Array[String]): Unit = {
