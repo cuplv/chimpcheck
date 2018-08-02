91d4c616da94c21174be2f4c4af09110427aaf35
Chance Roberts <roberts.chance@gmail.com>
2018-08-02 15:05:27 -0600

    A Better Docker Container
    This ends up running openstf/minicap for the Android emulator.

--- docker/start_docker.bash
+++ docker/start_docker.bash
@@ -2,8 +2,9 @@
 
 # Start the web server! (This is for hardware acceleration)
 bash /chimpcheck/ChimpWebDemo/start_web_server.bash & \
-sudo env "PATH=$PATH" emulator -avd test -noaudio -no-boot-anim -gpu off & wait
+(sleep 10 && sudo env "PATH=$PATH" emulator -avd test -noaudio -no-boot-anim -gpu swiftshader_indirect) & \
+bash /chimpcheck/ChimpWebDemo/start_adb_stream.bash & wait
 
-# Start the web servier! (Not hardware acceleration)
+# Start the web server! (Not hardware acceleration)
 # bash /chimpcheck/ChimpWebDemo/start_web_server.bash & \
 # sudo env "PATH=$PATH" emulator -avd test -noaudio -no-boot-anim -gpu off -accel off & wait
