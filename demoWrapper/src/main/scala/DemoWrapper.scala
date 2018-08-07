285b65ba540cbba2cd1917c87b7a90254812b821
PlatPlat48 <roberts.chance@gmail.com>
2018-08-07 15:46:47 -0600

    Added a bit more to the DemoWrapper
    Added slightly more to the demo wrapper to take care of both the stack
    trace and the base64 being on multiple lines.

--- demoWrapper/src/main/scala/DemoWrapper.scala
+++ demoWrapper/src/main/scala/DemoWrapper.scala
@@ -1,4 +1,7 @@
 import edu.colorado.plv.chimp.combinator.EventTrace
+import java.io.File
+
+import scala.io.Source
 
 /**
   * Created by chanceroberts on 8/7/18.
@@ -7,17 +10,35 @@ import edu.colorado.plv.chimp.combinator.EventTrace
 
 object DemoWrapper {
   def listOfCommands(): String = {
-    " decode base64\n" +
-    " "
+    " decode file.txt\n"
+  }
+
+  def getInfo(file: File, lookingFor: String, withNewLine: Boolean = false): String = {
+    val (_, executedTrace) = Source.fromFile(file).getLines().foldLeft((false, "")){
+      case ((false, x), l) if l.contains(lookingFor) =>
+        val newL = x+l.substring(l.indexOf('=')+1)
+        if (withNewLine) (true, s"$newL\n") else (true, newL)
+      case ((true, x), l) if !l.isEmpty =>
+        val newL = x+l
+        if (withNewLine) (true, s"$newL\n") else (true, newL)
+      case ((_, x), l) => (false, x)
+    }
+    executedTrace
   }
 
   def main(args: Array[String]): Unit = {
     args.length match{
-      case 0 | 1 => throw new Exception("Expected two arguments.\nList of commands: \n"+listOfCommands())
-      case _ => args(0) match{
-        case "decode" => println(EventTrace.fromBase64(args(1)))
-        case _ => throw new Exception("Unexpected command. \nList of commands: \n"+listOfCommands())
-      }
+      case 0 => throw new Exception("Expected an arguments.\nList of commands: \n"+listOfCommands())
+      case _ =>
+        //args(0) match{
+          //case "decode" =>
+        val a = new File(args(1))
+        val trace = getInfo(a, "INSTRUMENTATION_RESULT: ChimpDriver-ExecutedTrace=")
+        val stack = getInfo(a, "INSTRUMENTATION_STATUS: stack=", withNewLine = true)
+        println(s"ChimpCheck ended with a ${args(0).replace("ed","")} with trace ${EventTrace.fromBase64(trace)}!")
+        if (!stack.isEmpty) println(s"Stack:\n  $stack")
+          //case _ => ()
+      //}
     }
   }
 }
