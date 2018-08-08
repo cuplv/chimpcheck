9fb16fe49042bd2ca4cca0ec114d185d81779001
Chance Roberts <roberts.chance@gmail.com>
2018-08-08 13:08:28 -0600

    Generic Script Written
    Need to test generic script, next step is to merge with branch docker_container.

---
+++ ChimpWebDemo/example/runCommand.sh
@@ -0,0 +1,7 @@
+app1=$(cat ${1}Info.txt | head -n 1)
+app2=$(cat ${1}Info.txt | tail -n 1)
+adb -s emulator-5554 uninstall $app1
+adb -s emulator-5554 uninstall ${app1}.test
+adb -s emulator-5554 install $(dirname "$0")/$1/app-debug.apk
+adb -s emulator-5554 install $(dirname "$0")/$1/app-debug-androidTest.apk
+adb -s emulator-5554 shell am instrument -r -w -e debug false -e eventTrace $2 -e appPackageName $app2 -e class $app2.TestExpresso $app1.test/edu.colorado.plv.chimp.driver.ChimpJUnitRunner
