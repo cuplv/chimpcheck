140a9f7bce3e9ab8af01eba8174f03ed03e3cb00
PlatPlat48 <roberts.chance@gmail.com>
2018-08-13 14:15:15 -0600

    Fixed runCommand to query correctly.
    Now the server should be queried correctly.

--- ChimpWebDemo/example/runCommand.sh
+++ ChimpWebDemo/example/runCommand.sh
@@ -1,3 +1,3 @@
-app1=$(cat $(dirname "$0")/${3}Info.txt | head -n 1)
-app2=$(cat $(dirname "$0")/${4}Info.txt | tail -n 1)
+app1=$(cat $(dirname "$0")/${1}Info.txt | head -n 1)
+app2=$(cat $(dirname "$0")/${1}Info.txt | tail -n 1)
 curl -X POST "http://localhost:18010" -H "accept: application/json" -d '{"eventTrace": "'"${2}"'", "test": "'"${1}"'", "apk": "'"${app1}"'", "appPack": "'"${app2}"'" }'
