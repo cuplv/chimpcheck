1ef494f9c32ac65261fd0976bd2247a2abcfa939
PlatPlat48 <roberts.chance@gmail.com>
2018-08-13 11:30:13 -0600

    Bash inside Scala Now
    …Actually, this is probably a better idea.

--- ChimpDemoSrv/ChimpCheckEmulatorService/src/main/scala/ServerLogic.scala
+++ ChimpDemoSrv/ChimpCheckEmulatorService/src/main/scala/ServerLogic.scala
@@ -10,26 +10,48 @@ import scala.sys.process._
   */
 object ServerLogic {
   def runAnEmulator(queryStr: String, conf: Config): String = {
+    val chimpCheckLoc = conf.getString("chimpCheckAPKLoc")
     val json = queryStr.parseJson.asJsObject
     val test = json.fields.getOrElse("test",
-      throw new Exception("Unexpected JSON Format (Requires field test for the test to run.)"))
+      throw new Exception("Unexpected JSON Format (Requires field test for the test to run.)")) match{
+      case JsString(s) => s
+      case _ => throw new Exception("Unexpected JSON Format (Requires field test to be a string.")
+    }
     val packAPK = json.fields.getOrElse("appPack",
-      throw new Exception("Unexpected JSON Format (Requires field appPack for the app package name."))
+      throw new Exception("Unexpected JSON Format (Requires field appPack for the app package name.")) match{
+      case JsString(s) => s
+      case _ => throw new Exception("Unexpected JSON Format (Requires field packAPK to be a string.")
+    }
     val testAPK = json.fields.getOrElse("apk",
       throw new Exception("Unexpected JSON Format (Requires field apk for the package to install. " +
         "(Yes, this is different than the app package name...)")
-    )
+    ) match{
+      case JsString(s) => s
+      case _ => throw new Exception("Unexpected JSON Format (Requires field testAPK to be a string.")
+    }
     val eventTrace = json.fields.getOrElse("eventTrace",
       throw new Exception("Unexpected JSON Format (Requires field eventTrace for the Base64-encoded version of the event trace.)")
-    )
+    ) match{
+      case JsString(s) => s
+      case _ => throw new Exception("Unexpected JSON Format (Requires field eventTrace to be a string.")
+    }
     //Get the Emulator (Returns Host:Port)
     /*val newContainer = Http("???").postData(
       JsObject(Map("id" -> JsString(conf.getString("id")), "instances" -> JsNumber(1))).prettyPrint
     ).method("PATCH").header("accept", "application/json").asString.body.parseJson*/
-    val (newHost, adbPort, streamPort) = ("172.17.0.3", "5037", "9002") //Hardcoded for now.
+    val (newHost, adbPort, streamPort) = ("localhost", "5037", "9002") //Hardcoded for now.
     // Http("").asString.body
     //Yes, exprAPK and testAPK are two different things.
-    val chimpCheckReturn = s"bash runCommand.sh $newHost $adbPort $test $packAPK $testAPK $eventTrace".!!
+    //Here's the bash scripts!
+    s"adb -H $newHost -P $adbPort -s emulator-5554 uninstall $testAPK".!
+    s"adb -H $newHost -P $adbPort -s emulator-5554 uninstall $testAPK.test".!
+    s"adb -H $newHost -P $adbPort -s emulator-5554 install $chimpCheckLoc/$test/app-debug.apk".!
+    s"adb -H $newHost -P $adbPort -s emulator-5554 install $chimpCheckLoc/$test/app-debug-androidTest.apk".!
+    val chimpCheckReturn =
+      s"adb -H $newHost -P $adbPort -s emulator-5554 shell am instrument -r -w -e debug false " +
+        s"-e eventTrace $eventTrace -e appPackageName $packAPK -e class $packAPK.TestExpresso " +
+        s"$testAPK.test/edu.colorado.plv.chimp.driver.ChimpJUnitRunner".!!
+    //val chimpCheckReturn = s"bash runCommand.sh $newHost $adbPort $test $packAPK $testAPK $eventTrace".!!
     //Close the Emulator
     /*val done = Http("???").postData(
       //I'm not convinced that this is the correct way to do it...
