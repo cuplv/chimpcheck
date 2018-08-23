de6cc03f35850edae4b23612ceddd39759435935
PlatPlat48 <roberts.chance@gmail.com>
2018-08-23 14:24:01 -0600

    Reverted changes done by Docker Container

--- ChimpWebDemo/start_adb_stream.bash
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-
-# Following steps from github.com/openstf/minicap/example.
-# This uses the default web server on minicap currently.
-(cd /minicap && ./run.sh -P 320x480@320x480/0) & (cd /minicap/example && PORT=9002 node app.js) &
-(sleep 20 && adb forward tcp:1717 localabstract:minicap) & wait
