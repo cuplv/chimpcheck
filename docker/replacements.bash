32d92dc9ce02db462d84c96317112025395789c5
smover <sergio.mover@gmail.com>
2018-09-06 15:41:42 -0600

    minor

--- docker/replacements.bash
+++ docker/replacements.bash
@@ -1,12 +1,14 @@
 #!/bin/bash
 
-cat /chimpcheck/ChimpWebDemo/package.json | sed 's/localhost:9002/'"$STREAM_ADDRESS":"$STREAM_PORT"'/'  > /chimpcheck/ChimpWebDemo/package2.json
-cat /chimpcheck/ChimpWebDemo/package2.json > /chimpcheck/ChimpWebDemo/package.json
-rm /chimpcheck/ChimpWebDemo/package2.json
+ROOTWEB="/chimpcheck/ChimpWebDemo"
 
-cat /chimpcheck/ChimpWebDemo/client/src/Dropdown.js | sed 's/localhost:9002/'"$STREAM_ADDRESS":"$STREAM_PORT"'/' > /chimpcheck/ChimpWebDemo/client/src/App2.js
-cat /chimpcheck/ChimpWebDemo/client/src/App2.js > /chimpcheck/ChimpWebDemo/client/src/Dropdown.js
-rm /chimpcheck/ChimpWebDemo/client/src/App2.js
+cat ${ROOTWEB}/package.json | sed 's/localhost:9002/'"$STREAM_ADDRESS":"$STREAM_PORT"'/'  > ${ROOTWEB}/package2.json
+cat ${ROOTWEB}/package2.json > ${ROOTWEB}/package.json
+rm ${ROOTWEB}/package2.json
+
+cat ${ROOTWEB}/client/src/Dropdown.js | sed 's/localhost:9002/'"$STREAM_ADDRESS":"$STREAM_PORT"'/' > ${ROOTWEB}/client/src/App2.js
+cat ${ROOTWEB}/client/src/App2.js > ${ROOTWEB}/client/src/Dropdown.js
+rm ${ROOTWEB}/client/src/App2.js
 
 cat /minicap/example/public/index.html | sed 's/9002/'"$STREAM_ADDRESS":"$STREAM_PORT"'/' | sed "s/localhost/'+document.location.hostname+'/" > /minicap/example/public/index2.html
 cat /minicap/example/public/index2.html > /minicap/example/public/index.html
